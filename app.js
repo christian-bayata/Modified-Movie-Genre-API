cbb33d1f854cb09347e7f94639a79250d73c138b
christian-bayata <franksagie1@gmail.com>
2021-07-06 19:26:54 +0100

    Movies API with winston handled exceptions

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ mongoose.connect('mongodb://localhost:27017/testDB', {
 .then(() => console.log("Connected to mongoDB..."))
 .catch((err) => console.log("Not connected to mongoDB..."));
 
+winston.handleExceptions(new winston.transports.File({ filename: uncaughtExceptions.log }));
+
+process.on('unhandledRejection', (ex) => {
+   throw ex;
+});
+
+const p = Promise.reject(new Error("Something failed terribly"));
+p.then(() => console.log('Done'));
+
 winston.add(winston.transports.File, { filename: 'logFile.log' });
 winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost:27017/testDB' });
 
